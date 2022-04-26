48a704f74a632d30f373025616c9c848fc41ba72
Nicolas Boute <4249@holbertonschool.com>
2022-04-26 15:26:41 +0200

    added traceback for invalid argument

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    if (type(number) != int):
+        raise TypeError
     digit = repr(number)[-1]
     print('{}'.format(digit), end='')
     return digit
