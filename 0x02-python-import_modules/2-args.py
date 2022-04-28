ea6af6a2232e9feb7cbea338ba8002e7b097fa8f
Nicolas Boute <4249@holbertonschool.com>
2022-04-28 13:11:07 +0200

    added missing character in output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if nb == 0:
         print('0 arguments.')
     else:
-        print(f'{nb} argument{"s" if nb != 1 else ""}')
+        print(f'{nb} argument{"s" if nb != 1 else ""}:')
         for i in range(nb):
             print(f'{i + 1}: {sys.argv[i + 1]}')
