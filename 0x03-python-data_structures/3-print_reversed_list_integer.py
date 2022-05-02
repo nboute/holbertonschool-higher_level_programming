74a0d3139a2124893340eacdbf488cc15cc0ba49
Nicolas Boute <4249@holbertonschool.com>
2022-05-02 18:05:57 +0200

    readded clause guard

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     my_list.reverse()
     for elem in my_list:
         print(f'{elem:d}')
