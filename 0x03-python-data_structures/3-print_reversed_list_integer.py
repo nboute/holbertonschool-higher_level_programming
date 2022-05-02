179d0470096ee81a75bfb4fa5072f9921e84284f
Nicolas Boute <4249@holbertonschool.com>
2022-05-02 18:04:46 +0200

    added clause guard in list printing function

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for elem in reversed(range(len(my_list))):
-        print(f'{my_list[elem]:d}')
+    if my_list is None:
+        return
+    my_list.reverse()
+    for elem in my_list:
+        print(f'{elem:d}')
