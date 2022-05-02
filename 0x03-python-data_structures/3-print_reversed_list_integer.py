fae4d06f67d61fa31a821f0f5ca813813dc3f081
Nicolas Boute <4249@holbertonschool.com>
2022-05-02 18:05:20 +0200

    removed clause guard to tell if checker verified it

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list is None:
-        return
     my_list.reverse()
     for elem in my_list:
         print(f'{elem:d}')
