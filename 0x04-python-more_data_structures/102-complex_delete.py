577cb1f7c86da4ce175ac6521a00fe513dbda9de
Nicolas Boute <4249@holbertonschool.com>
2022-05-04 23:06:29 +0200

    fixed delete not modifying original dict

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-    mydict = {key: val for key, val in a_dictionary.items() if val != value}
-    return mydict
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            a_dictionary.pop(key)
+    return a_dictionary
