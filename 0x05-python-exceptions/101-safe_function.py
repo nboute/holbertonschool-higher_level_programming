7406d71035ac094131c06e129e7a418a55356bc2
Nicolas Boute <4249@holbertonschool.com>
2022-05-16 12:14:14 +0200

    fixed typo

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     res = None
     try:
-        res = fct(args[0], args[1])
+        res = fct(*args)
         return res
     except Exception as e:
         sys.stderr.write(f'Exception: {e}\n')
