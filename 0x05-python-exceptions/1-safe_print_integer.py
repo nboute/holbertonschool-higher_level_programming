77a85e266ff749c273b598788430c5cd766905b7
Nicolas Boute <4249@holbertonschool.com>
2022-05-16 12:07:47 +0200

    replaced f string with format for requirements

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        print(f'{value:d}')
+        print('{:d}'.format(value))
     except (TypeError, ValueError) as e:
         return False
     return True
