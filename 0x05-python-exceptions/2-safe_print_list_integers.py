25cb407a78e65cff0f56afa7a495fbd3aa4935a1
Nicolas Boute <4249@holbertonschool.com>
2022-05-16 12:10:00 +0200

    replaced f string with format for requirements

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     j = x
     for i in range(x):
         try:
-            print(f'{my_list[i]:d}', end='')
+            print('{:d}'.format(my_list[i]), end='')
         except (ValueError, TypeError) as e:
             j -= 1
             pass
