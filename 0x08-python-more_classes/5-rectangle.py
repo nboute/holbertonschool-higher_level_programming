b848ff8f18224c857fd3c5045c0da128c7cae37a
Nicolas Boute <4249@holbertonschool.com>
2022-05-24 09:55:46 +0200

    fixed yet another typo

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -87,9 +87,9 @@ class Rectangle():
 
         """
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -141,7 +141,7 @@ class Rectangle():
         """
         s = ""
         if self.__height == 0 or self.__width == 0:
-            return "\n"
+            return ""
         for i in range(self.__height):
             for j in range(self.__width):
                 s += '#'
