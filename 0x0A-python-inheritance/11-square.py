39f53b0151f27afbccd91ab69a6bb9a058738d3b
Nicolas Boute <4249@holbertonschool.com>
2022-05-30 19:08:36 +0200

    patching bit by bit with working code

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""11-square.py
-
-This module contains a class Square that inherits
-from Rectangle (9-rectangle.py):
-
-Instantiation with size: def __init__(self, size)::
-    - size must be private. No getter or setter
-    - size must be a positive integer, validated by integer_validator
-
-the area() method must be implemented
-
-print() should print, and str() should return,
-the square description: [Square] <width>/<height>
+"""Square inherits from Rectangle which inherits from BaseGeometry
 """
-
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a square. Inherits from Rectangle class"""
+    """Defines a square"""
 
     def __init__(self, size):
         """Initializes a Square object when it is instanciated"""
-        super().integer_validator("size", size)
         super().__init__(size, size)
+        super().integer_validator("size", size)
         self.__size = size
 
+    def area(self):
+        """Returns area of the Square object"""
+        return self.__size ** 2
+
     def __str__(self):
-        """Returns a string containing the square"""
-        return '[Square] {}/{}'.format(self.__size, self.__size)
+        return f'[Square] {self.__size}/{self.__size}'
