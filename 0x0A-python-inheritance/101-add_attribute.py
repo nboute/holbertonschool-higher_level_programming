a50f8cf8c25699c797184e5d8ea5499993a051cb
Nicolas Boute <4249@holbertonschool.com>
2022-05-30 18:47:34 +0200

    added missing quotes on string

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,20 +12,6 @@ Raise a TypeError exception, with the message 'can't add new attribute'
 
 def add_attribute(obj, name, value):
     """Adds attribute to an object if possible, otherwise raises exception"""
-    if (hasattr(obj, __dict__) is False):
+    if (hasattr(obj, "__dict__") is False):
         raise AttributeError("can't add new attribute")
     setattr(obj, name, value)
-
-class MyClass():
-    pass
-
-mc = MyClass()
-add_attribute(mc, "name", "John")
-print(mc.name)
-
-try:
-    a = "My String"
-    add_attribute(a, "name", "Bob")
-    print(a.name)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
