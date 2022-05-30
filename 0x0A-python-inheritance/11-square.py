3807a5846a43f6edd162d01d6e312ec7ae4e895d
Nicolas Boute <4249@holbertonschool.com>
2022-05-30 19:07:26 +0200

    replaced f string with .format

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-"""Square inherits from Rectangle which inherits from BaseGeometry
+"""11-square.py
+
+This module contains a class Square that inherits
+from Rectangle (9-rectangle.py):
+
+Instantiation with size: def __init__(self, size)::
+    - size must be private. No getter or setter
+    - size must be a positive integer, validated by integer_validator
+
+the area() method must be implemented
+
+print() should print, and str() should return,
+the square description: [Square] <width>/<height>
 """
-Rectangle = __import__("9-rectangle").Rectangle
+
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a square"""
+    """Defines a square. Inherits from Rectangle class"""
 
     def __init__(self, size):
-        super().__init__(size, size)
+        """Initializes a Square object when it is instanciated"""
         super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
-    def area(self):
-        """Returns area of the Square object"""
-        return self.__size ** 2
-
     def __str__(self):
-        return f'[Square] {self.__size}/{self.__size}'
+        """Returns a string containing the square"""
+        return '[Square] {}/{}'.format(self.__size, self.__size)
