b33d00e67dcb9631aa123e88423346ec88377750
Nicolas Boute <4249@holbertonschool.com>
2022-05-30 15:13:36 +0200

    added missing jsdoc

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,17 +3,25 @@
 import dis
 import math
 
+"""103-magic_class.py
+
+This module contains a class MagicClass that defines a circle
+"""
 
 class MagicClass():
+    """Defines a circle"""
 
     def __init__(self, radius):
+        """Initalizes a MagicClass object when instantiated"""
         self._MagicClass__radius = 0
         if type(radius) is not int or type(radius) is not float:
             raise TypeError("radius must be a number")
         self._MagicClass__radius = radius
 
     def area(self):
+        """Returns area of MagicClass object"""
         return self._MagicClass__radius ** 2 * math.pi()
 
     def circumference(self):
+        """Returns circumference of MagicClass object"""
         return 2 * math.pi * self._MagicClass__radius
