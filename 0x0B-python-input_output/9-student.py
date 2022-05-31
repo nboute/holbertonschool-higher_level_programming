1f3fe191a8e6643d14dd985245b300bda6fc4be1
Nicolas Boute <4249@holbertonschool.com>
2022-05-31 22:02:06 +0200

    fixed documentation line too long

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,8 +7,8 @@ last_name
 age
 Instantiation with first_name, last_name and age:
     def __init__(self, first_name, last_name, age):
-Public method def to_json(self): that retrieves a dictionary representation of
-a Student instance (same as 8-class_to_json.py)
+Public method def to_json(self): that retrieves a dictionary
+representation of a Student instance (same as 8-class_to_json.py)
 """
 
 import json
