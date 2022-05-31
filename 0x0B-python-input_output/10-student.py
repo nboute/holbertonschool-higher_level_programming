de424b0add75430d6688b723a6b1d9319743ce28
Nicolas Boute <4249@holbertonschool.com>
2022-05-31 22:44:54 +0200

    removed unused import

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""This module contains a class Student that defines a student
 """
-
-import json
+This module contains a class Student that defines a student
+"""
 
 
 class Student():
-    """Student class
-    Contains a student's personal info
-    """
+    """Student class"""
 
     def __init__(self, first_name, last_name, age):
+        """Initialization of student class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
-"""This module contains a class Student that defines a student by:
-
-Public instance attributes:
-first_name
-last_name
-age
-Instantiation with first_name, last_name and age:
-    def __init__(self, first_name, last_name, age):
-Public method def to_json(self): that retrieves a dictionary
-representation of a Student instance (same as 8-class_to_json.py)
 """
-
-import json
+This module contains a class Student that defines a student
+"""
 
 
 class Student():
-    """Student class
-    Contains a student's personal info
-    """
+    """Student class"""
 
     def __init__(self, first_name, last_name, age):
+        """Initialization of student class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
