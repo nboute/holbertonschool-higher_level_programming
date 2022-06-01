86bb174a56fb75c220d11535e230257bbf9779fc
Nicolas Boute <4249@holbertonschool.com>
2022-06-01 10:39:16 +0200

    added unittest assignment of project 0x07

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([...])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Unittest class for function max_integer"""
+
+    def test_max_integer(self):
+        """Tests returns values for normal and specific cases"""
+        self.assertEqual(max_integer([0, 1, 2]), 2)
+        self.assertEqual(max_integer([2, 1, 0]), 2)
+        self.assertEqual(max_integer([0, 99999999, 2]), 99999999)
+        self.assertEqual(max_integer([0, 1, 2]), 2)
+        self.assertEqual(max_integer([-99999, 2, 0]), 2)
+        self.assertEqual(max_integer([]), None)
