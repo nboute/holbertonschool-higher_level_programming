f40c69e8588ef3d87fc5023008bdff9dda5aa226
Nicolas Boute <4249@holbertonschool.com>
2022-07-05 23:22:59 +0200

    added missing use of database

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,7 @@
 -- This script converts a database to UTF8
+
+-- Select databse htbn_0c_0
+USE hbtn_0c_0;
 -- Converts database to utf8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Converts table first_table to utf8
