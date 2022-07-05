f5f500b6b1a29e84291c2856a47de83fa580ca8c
Nicolas Boute <4249@holbertonschool.com>
2022-07-05 23:24:11 +0200

    removed useless statement in script

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -7,6 +7,3 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Converts table first_table to utf8
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Converts field name to utf8
-ALTER TABLE first_table
-MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
