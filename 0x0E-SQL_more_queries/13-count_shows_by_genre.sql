cf70510efbde99713959125c299f495b1155da50
Nicolas Boute <4249@holbertonschool.com>
2022-07-06 15:27:27 +0200

    added missing alias for column printing

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- This script lists all genres and displays the number of shows for each
 
 -- List all genres
-SELECT a.name, COUNT(b.genre_id) AS number_of_shows
+SELECT a.name AS genre, COUNT(b.genre_id) AS number_of_shows
 FROM tv_genres a
 INNER JOIN tv_show_genres b
 ON a.id = b.genre_id
