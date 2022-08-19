ab2ce7589f715cc6e3ff772fc5031f301ca68f4a
Nicolas Boute <4249@holbertonschool.com>
2022-08-19 12:56:04 +0200

    fixed typo

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ from urllib.request import urlopen
 with urlopen("https://intranet.hbtn.io/status") as response:
     print("Body Response:")
     body = response.read()
-    print("\t - type: {}".format(type(body)))
-    print("\t - content: {}".format(body))
-    print("\t - utf8 content: {}".format(body.decode('utf-8')))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
