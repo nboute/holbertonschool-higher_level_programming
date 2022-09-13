69a376bc15e585502c618edded489287017f1454
Nicolas Boute <4249@holbertonschool.com>
2022-09-13 13:45:17 +0200

    fixed typo

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const axios = require('axios');
-
+const argv = process.argv;
 const users = {};
 
 axios
-  .get('https://jsonplaceholder.typicode.com/todos')
+  .get(argv[2])
   .then(res => {
     res.data.forEach(function (task) {
       if (task.completed === true) {
