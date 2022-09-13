2083038ff8d1003e1c344dd0a43758a6e65787c9
Nicolas Boute <4249@holbertonschool.com>
2022-09-13 13:44:09 +0200

    fixed semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,7 +9,7 @@ axios
   .then(res => {
     res.data.forEach(function (task) {
       if (task.completed === true) {
-        if (users[task.userId] == undefined) {
+        if (users[task.userId] === undefined) {
           users[task.userId] = 0;
         }
         users[task.userId] += 1;
