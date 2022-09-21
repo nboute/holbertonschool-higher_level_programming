b0764b27b22b216259fe01fca4c19ab14122be98
Nicolas Boute <4249@holbertonschool.com>
2022-09-21 15:37:59 +0200

    removed debug lines

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,16 +1,13 @@
 $(document).ready(function () {
   $('#add_item').click(function () {
-    console.log('A');
     $('ul.my_list').prepend('<li>Item</li>');
   });
 
   $('#remove_item').click(function () {
-    console.log('B');
     $('ul.my_list li').last().remove();
   });
 
   $('#clear_list').click(function () {
-    console.log('C');
     $('ul.my_list').empty();
   });
 });
